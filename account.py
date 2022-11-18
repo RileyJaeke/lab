4e1c3c6657021542f9a1d487fb0a484dd12fd520
RileyJaeke <118500890+RileyJaeke@users.noreply.github.com>
2022-11-17 22:57:04 -0600

    Add files via upload

---
+++ account.py
@@ -0,0 +1,30 @@
+class Account:
+
+    def __init__(self, name):
+
+        self.account_name = name
+        self.account_balance = 0
+
+    def deposit(self, amount):
+
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdrawal(self, amount):
+
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+
+        return self.account_balance
+
+    def get_name(self):
+
+        return self.account_name
